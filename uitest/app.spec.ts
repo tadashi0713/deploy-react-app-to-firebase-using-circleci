69a1d02a54500e205ea4cf51e62179255f79b1fe
Tadashi Nemoto <tadashi.nemoto@exwzd.com>
2021-03-22 19:33:34 +0900

    Add testcafe to ci

--- uitest/app.spec.ts
+++ uitest/app.spec.ts
@@ -5,5 +5,10 @@ fixture `App`
     .page `http://localhost:3000`;
 
 test('Click event test', async t => {
+  const button = Selector('button').withAttribute('data-testid', 'Button')
+  const text = Selector('p').withAttribute('data-testid', 'Text')
 
+  await t
+    .click(button)
+    .expect(text.innerText).eql("Hello")
 });
